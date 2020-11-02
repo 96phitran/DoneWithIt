bedee9465c83b501b0de04ece9c1370ea0c3ea07
96phitran <s3636649@rmit.edu.vn>
2020-11-02 16:24:06 +0700

    Add Components and Screens

--- App.js
+++ App.js
@@ -8,8 +8,14 @@ import {
   StatusBar,
   Image,
 } from "react-native";
+
+import ViewImageScreen from "./app/screen/ViewImageScreen";
 import WelcomeScreen from "./app/screen/WelcomeScreen";
+import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import ListingDetaisScreen from "./app/screen/ListingDetaisScreen";
+import MessagesScreen from "./app/screen/MessagesScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <MessagesScreen />;
 }
