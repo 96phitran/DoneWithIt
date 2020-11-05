90c7e8321b9d1923c84e57102c681d5a30696897
96phitran <s3636649@rmit.edu.vn>
2020-11-05 10:54:40 +0700

    Add Login Screen

--- App.js
+++ App.js
@@ -1,37 +1,11 @@
 import React, { useState } from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-  StatusBar,
-  Image,
-  TextInput,
-  Switch,
-} from "react-native";
 
-import ViewImageScreen from "./app/screen/ViewImageScreen";
-import WelcomeScreen from "./app/screen/WelcomeScreen";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import ListingDetaisScreen from "./app/screen/ListingDetaisScreen";
-import MessagesScreen from "./app/screen/MessagesScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screen/AccountScreen";
-import ListingScreen from "./app/screen/ListingScreen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
+import LoginScreen from "./app/screen/LoginScreen";
 
 export default function App() {
-  const [isNew, setIsNew] = useState(false);
-
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
-    <Screen>
-      <AppPicker icon="apps" placeholder="Category" />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
+    <LoginScreen />
   );
 }
