aa1971a1a790540c858702af887fbac1e3d1e512
96phitran <s3636649@rmit.edu.vn>
2020-11-06 13:05:15 +0700

    Add ImageInput and ImageInputList components

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
+import Screen from "./app/components/Screen";
 import ListingEditScreen from "./app/screen/ListingEditScreen";
 import MessagesScreen from "./app/screen/MessagesScreen";
+import { Button, Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
-  return <ListingEditScreen/>;
+  const [imageUris, setImageUris] = useState([]);
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
+  };
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri != uri));
+  };
+
+  return (
+    <Screen>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+    </Screen>
+  );
 }
