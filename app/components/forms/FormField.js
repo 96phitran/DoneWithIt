99dc402f7be011c25496ea1a6d14e642733756c8
96phitran <s3636649@rmit.edu.vn>
2020-11-12 16:21:25 +0700

    Handling upload listing

--- app/components/forms/FormField.js
+++ app/components/forms/FormField.js
@@ -5,12 +5,19 @@ import AppTextInput from "../TextInput";
 import ErrorMessage from "./ErrorMessage";
 
 function AppFormField({ name, width, ...otherProps }) {
-  const { handleChange, errors, setFieldTouched, touched } = useFormikContext();
+  const {
+    errors,
+    setFieldTouched,
+    setFieldValue,
+    touched,
+    values,
+  } = useFormikContext();
   return (
     <>
       <AppTextInput
         onBlur={() => setFieldTouched(name)}
-        onChangeText={handleChange(name)}
+        onChangeText={(text) => setFieldValue(name, text)}
+        value={values[name]}
         width={width}
         {...otherProps}
       />
